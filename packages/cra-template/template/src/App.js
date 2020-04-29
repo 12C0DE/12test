00c79c19ce9d81f9b2d25776bd798bb5d2bcca45
12C0DE <rubenhnt@gmail.com>
2020-04-29 16:51:00 -0500

    update toml 2nd

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          test this 2x again changing toml
+          test this 2x again changing toml twice
         </a>
       </header>
     </div>
